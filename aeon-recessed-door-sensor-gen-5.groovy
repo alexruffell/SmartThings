aa4adbe169f3fabab8e09e593c5081d731a9a7f9
alexruffell <alexruffell@users.noreply.github.com>
2017-07-18 22:24:26 -0500

    Update aeon-recessed-door-sensor-gen-5.groovy

--- aeon-recessed-door-sensor-gen-5.groovy
+++ aeon-recessed-door-sensor-gen-5.groovy
@@ -1,6 +1,7 @@
 /**
  *  Aeon Recessed Door Sensor
  *  Based On: Z-Wave Door/Window Sensor
+ *  Version 1.1
  *
  *  Author: Mike
  *  Modified by: Mike
@@ -13,7 +14,7 @@
 // for the UI
 metadata {
 	// Automatically generated. Make future change here.
-	definition (name: "Aeon Recessed Door Sensor - Battery Fix v2", namespace: "jabbera", author: "Mike") {
+	definition (name: "Aeon Recessed Door Sensor", namespace: "jabbera", author: "Mike") {
 		capability	"Contact Sensor"
 		capability	"Sensor"
 		capability	"Battery"
